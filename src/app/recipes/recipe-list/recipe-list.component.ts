8807dd3ac565a12b37e01b5004e76e01d5549aa3
hasan <hasanbatis440@gmail.com>
2021-07-25 19:31:47 +0300

    adding components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test','test desc','https://www.themediterraneandish.com/wp-content/uploads/2020/02/falafel-recipe-10.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit(){
+  }
+
+}
