e72adc3e59724ecbbbfae1f25c4b4019e8752ad6
hasan <hasanbatis440@gmail.com>
2021-07-29 19:04:26 +0300

    recipe component and recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Output , EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,11 +6,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test','test desc','https://www.themediterraneandish.com/wp-content/uploads/2020/02/falafel-recipe-10.jpg')
+    new Recipe('A test','test desc','https://www.themediterraneandish.com/wp-content/uploads/2020/02/falafel-recipe-10.jpg'),
+    new Recipe('A another','test desc','https://www.themediterraneandish.com/wp-content/uploads/2020/02/falafel-recipe-10.jpg')
   ];
   constructor() { }
 
   ngOnInit(){
+  }
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 }
